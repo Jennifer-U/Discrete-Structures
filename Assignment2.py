4eca3a910a7572943afed359fad1944a32f70a44
Jennifer Umana <89053057+Jennifer-U@users.noreply.github.com>
2026-02-06 01:16:29 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,9 +1,7 @@
-# Queens College
-# Discrete Structures (CSCI 220)
+# Discrete Structures 
 # Winter 2024
 # Assignment 2: "Boolean Algebra"
-# name Jennifer Umana
-# Collaborated with Class
+
 
 import inspect
 import pandas as pd
